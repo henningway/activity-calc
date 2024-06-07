9496b43cf3fdd2d3318f1f5e09c6081742f25342
Henning Schindler <henning.schindler@peerox.de>
2024-06-07 14:11:27 +0200

    group by data and analyze

--- calc.js
+++ calc.js
@@ -1,4 +1,5 @@
 const fs = require("node:fs");
+const R = require("ramda");
 
 /**
  * Augments the activity entries in the file at given path with durations.
@@ -23,7 +24,7 @@ function writeActivityDurationToFile(path) {
  * @return {string[]}
  */
 function augmentWithDurations(lines) {
-  return addDurationSuffixes(lines, durationPerLine(lines));
+  return addDurationSuffixes(lines, durationPerLine(lines, true));
 }
 
 /**
@@ -48,7 +49,7 @@ function addDurationSuffixes(lines, durations) {
  * @param {string[]} lines
  * @return {string[]}
  */
-function durationPerLine(lines) {
+function durationPerLine(lines, skipIfHasDiff = false) {
   return lines.map((line, index) => {
     const nextLine = index + 1 >= lines.length ? null : lines[index + 1];
 
@@ -56,7 +57,7 @@ function durationPerLine(lines) {
 
     const hasDiff = line.match(/\([0-9]*\)$/)?.length > 0;
 
-    if (hasDiff) return;
+    if (skipIfHasDiff && hasDiff) return;
 
     const time = line.substring(0, 4);
     const nextTime = nextLine.substring(0, 4);
@@ -86,6 +87,37 @@ function durationPerLine(lines) {
 }
 
 /**
+ * Matches the timestamp in given line.
+ */
+function matchTimestamp(line) {
+  return R.match(/^([0-9]{4})/, line);
+}
+
+/**
+ * Extracts the timestamp in a given line.
+ */
+function extractTimestamp(line) {
+  return matchTimestamp(line)?.[1];
+}
+
+/**
+ * Matches the label in givne line.
+ */
+function matchLabel(line) {
+  // ^[0-9]{4} ([^(\()]*)\s?(\([0-9]*\))?$
+  return R.match(/^[0-9]{4} ([^(\()]*)\s?(\([0-9]*\))?$/, line);
+}
+
+/**
+ * Extracts the label in a given line.
+ */
+function extractLabel(line) {
+  const result = matchLabel(line)?.[1];
+  if (result === undefined) return result;
+  return R.trim(result);
+}
+
+/**
  * Calculates the duration in minutes between two time strings in HH:MM format.
  */
 function timeDifferenceInMinutes(time1, time2) {
@@ -99,6 +131,47 @@ function timeDifferenceInMinutes(time1, time2) {
   const differenceInMinutes = time2InMinutes - time1InMinutes;
 
   return differenceInMinutes;
+}
+
+/**
+ * Groups lines by date.
+ */
+function groupByDate(lines) {
+  const dateRegEx = /^# ([0-9]{2}\.[0-9]{2}\.[0-9]{4})/;
+  const matchDate = R.match(dateRegEx);
+
+  const dates = R.map(
+    (dateLine) => matchDate(dateLine)[1],
+    R.filter(R.test(dateRegEx), lines)
+  );
+  const groups = R.splitWhenever(R.test(dateRegEx), lines);
+
+  return R.map(
+    (entry) => entry[1],
+    R.indexBy((entry) => entry[0], R.zip(dates, groups))
+  );
+}
+
+/**
+ * Groups lines by date and provides meta information for each line (duration, label, timestamp).
+ */
+function groupByDateAndAnalyze(lines) {
+  return mapGroup(
+    ([text, duration]) => ({
+      text,
+      duration,
+      timestamp: extractTimestamp(text),
+      label: extractLabel(text),
+    }),
+    R.map((lines) => R.zip(lines, durationPerLine(lines)), groupByDate(lines))
+  );
+}
+
+/**
+ * Maps a function to all lines in all groups.
+ */
+function mapGroup(fn, groups) {
+  return R.map(R.map(fn), groups);
 }
 
 /**
@@ -127,6 +200,11 @@ const isNumber = (v) => !isNaN(Number(v));
 writeActivityDurationToFile("./activity.md");
 
 module.exports = {
+  analyzeInput: groupByDateAndAnalyze,
   augmentWithDurations,
-  durationPerLine
+  durationPerLine,
+  extractLabel,
+  extractTimestamp,
+  groupByDate,
+  mapGroup,
 };
--- calc.test.js
+++ calc.test.js
@@ -1,37 +1,56 @@
 import { it, expect } from "vitest";
-import { augmentWithDurations, durationPerLine } from "./calc";
+import {
+  analyzeInput,
+  augmentWithDurations,
+  durationPerLine,
+  extractLabel,
+  extractTimestamp,
+  groupByDate,
+  mapGroup,
+} from "./calc";
+import { map, zip } from "ramda";
 
 const input = [
-  "# 05.06.2024",
+  "# 28.05.2024",
   "",
+  "0950 Ankommen",
+  "1000 Daily (10)",
+  "1010 Responsive Ereignis-Kacheln",
+  "1200 Mittag",
+  "1240 Responsive Ereignis-Kacheln",
+  "1700 Ende",
+  "",
+  "# 27.05.2024",
+  "",
+  "0950 Gitlab/Mails",
   "1000 Daily",
-  "1010 Gitlab/Mails",
-  "1035 Responsive Ereignis-Kacheln",
-  "1100 Meeting Strategie E2E-Tests",
-  "1205 Mittag",
-  "1255 FASÖ Review",
-  "1300 Gitlab/Mails",
-  "1310 Responsive Ereignis-Kacheln",
-  "1540 FASÖ Review",
-  "1545 Gitlab/Mails",
-  "1630 Ende",
+  "1015 Labern",
+  "1030 Dev-Meeting",
+  "1215 Mittag",
+  "1250 Responsive Ereignis-Kacheln",
+  "1600 Ende",
 ];
 
 it("can add durations to activities", () => {
   expect(augmentWithDurations(input)).toStrictEqual([
-    "# 05.06.2024",
+    "# 28.05.2024",
     "",
+    "0950 Ankommen (10)",
     "1000 Daily (10)",
-    "1010 Gitlab/Mails (25)",
-    "1035 Responsive Ereignis-Kacheln (25)",
-    "1100 Meeting Strategie E2E-Tests (65)",
-    "1205 Mittag (50)",
-    "1255 FASÖ Review (5)",
-    "1300 Gitlab/Mails (10)",
-    "1310 Responsive Ereignis-Kacheln (150)",
-    "1540 FASÖ Review (5)",
-    "1545 Gitlab/Mails (45)",
-    "1630 Ende",
+    "1010 Responsive Ereignis-Kacheln (110)",
+    "1200 Mittag (40)",
+    "1240 Responsive Ereignis-Kacheln (260)",
+    "1700 Ende",
+    "",
+    "# 27.05.2024",
+    "",
+    "0950 Gitlab/Mails (10)",
+    "1000 Daily (15)",
+    "1015 Labern (15)",
+    "1030 Dev-Meeting (105)",
+    "1215 Mittag (35)",
+    "1250 Responsive Ereignis-Kacheln (190)",
+    "1600 Ende",
   ]);
 });
 
@@ -40,15 +59,110 @@ it("can provide duration per line", () => {
     undefined,
     undefined,
     10,
-    25,
-    25,
-    65,
-    50,
-    5,
     10,
-    150,
-    5,
-    45,
+    110,
+    40,
+    260,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    10,
+    15,
+    15,
+    105,
+    35,
+    190,
     undefined,
   ]);
+});
+
+it("can group by date", () => {
+  expect(groupByDate(input)).toStrictEqual({
+    "27.05.2024": [
+      "",
+      "0950 Gitlab/Mails",
+      "1000 Daily",
+      "1015 Labern",
+      "1030 Dev-Meeting",
+      "1215 Mittag",
+      "1250 Responsive Ereignis-Kacheln",
+      "1600 Ende",
+    ],
+    "28.05.2024": [
+      "",
+      "0950 Ankommen",
+      "1000 Daily (10)",
+      "1010 Responsive Ereignis-Kacheln",
+      "1200 Mittag",
+      "1240 Responsive Ereignis-Kacheln",
+      "1700 Ende",
+      "",
+    ],
+  });
+});
+
+it("can group and analyze (provide meta information to each line) the input", () => {
+  expect(analyzeInput(input)).toStrictEqual({
+    "27.05.2024": [
+      { text: "", label: undefined, duration: undefined, timestamp: undefined },
+      {
+        text: "0950 Gitlab/Mails",
+        label: "Gitlab/Mails",
+        duration: 10,
+        timestamp: "0950",
+      },
+      { text: "1000 Daily", label: "Daily", duration: 15, timestamp: "1000" },
+      { text: "1015 Labern", label: "Labern", duration: 15, timestamp: "1015" },
+      {
+        text: "1030 Dev-Meeting",
+        label: "Dev-Meeting",
+        duration: 105,
+        timestamp: "1030",
+      },
+      { text: "1215 Mittag", label: "Mittag", duration: 35, timestamp: "1215" },
+      {
+        text: "1250 Responsive Ereignis-Kacheln",
+        label: "Responsive Ereignis-Kacheln",
+        duration: 190,
+        timestamp: "1250",
+      },
+      {
+        text: "1600 Ende",
+        label: "Ende",
+        duration: undefined,
+        timestamp: "1600",
+      },
+    ],
+    "28.05.2024": [
+      { text: "", label: undefined, duration: undefined, timestamp: undefined },
+      {
+        text: "0950 Ankommen",
+        label: "Ankommen",
+        duration: 10,
+        timestamp: "0950",
+      },
+      { text: "1000 Daily (10)", label: "Daily", duration: 10, timestamp: "1000" },
+      {
+        text: "1010 Responsive Ereignis-Kacheln",
+        label: "Responsive Ereignis-Kacheln",
+        duration: 110,
+        timestamp: "1010",
+      },
+      { text: "1200 Mittag", label: "Mittag", duration: 40, timestamp: "1200" },
+      {
+        text: "1240 Responsive Ereignis-Kacheln",
+        label: "Responsive Ereignis-Kacheln",
+        duration: 260,
+        timestamp: "1240",
+      },
+      {
+        text: "1700 Ende",
+        label: "Ende",
+        duration: undefined,
+        timestamp: "1700",
+      },
+      { text: "", label: undefined, duration: undefined, timestamp: undefined },
+    ],
+  });
 });
